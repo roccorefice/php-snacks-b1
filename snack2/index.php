b44bec5408f42a8da8db4e1c60c3f5a0d96da4bc
Rocco Orefice <rocco.orefice92@gmail.com>
2021-01-29 17:14:05 +0100

    es 2 con utilizzo delle funz di php (strlen, strpos, is_numeric)

--- snack2/index.php
+++ snack2/index.php
@@ -7,43 +7,13 @@ conosciamo nella documentazione) che:
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php
-$matches = [
-    [
-        'match' =>
-        [
-            'Casa' => 'Olimpia Milano',
-            'Ospiti' => 'Cantù',
-            'punteggio' => '89 - 90',
-        ]
-    ],
-    [
-        'match' =>
-        [
-            'Casa' => 'Carpisa Napoli',
-            'Ospiti' => 'Energia Trentino',
-            'punteggio' => '86 - 58',
-        ]
-    ],
-    [
-        'match' =>
-        [
-            'Casa' => 'Armani Milano',
-            'Ospiti' => 'Virtus Bologna',
-            'punteggio' => '77 - 82',
-        ]
-    ],
-    [
-        'match' =>
-        [
-            'Casa' => 'De Longhi Treviso',
-            'Ospiti' => 'Fortitudo Bologna',
-            'punteggio' => '87 - 97',
-        ]
-    ],
-];
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
 
-for ($i = 0; $i < count($matches); ++$i) { 
-    echo "{$matches[$i]['match']['Casa']} - {$matches[$i]['match']['Ospiti']} : {$matches[$i]['match']['punteggio']} <br>";
-    
-};
+if(strlen($name) > 3 && strpos($mail, '@') !== false && strpos($mail, ".") !== false && is_numeric($age)){
+echo "Accesso riuscito";
+} else {
+    echo "Accesso negato";
+}
 ?>
